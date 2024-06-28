acd348667dafd80372c1499ceac5d23812999015
Zuri Pabon <reach.zuripabon@gmail.com>
2024-06-28 23:03:33 +0200

    fix: absolute path with ts-jest

--- apps/monolito/jest.config.js
+++ apps/monolito/jest.config.js
@@ -1,6 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const backConfig = require('@repo/jest-config/back/jest.config');
+const { pathsToModuleNameMapper } = require('ts-jest');
 
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig.json');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  ...backConfig
+  ...backConfig,
+  roots: ['.'],
+  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
--- packages/jest-config/back/jest.config.js
+++ packages/jest-config/back/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  cacheDirectory: '.tmp/jestCache'
+  cacheDirectory: '.tmp/jestCache',
+  moduleDirectories: ['node_modules', 'src']
 };
