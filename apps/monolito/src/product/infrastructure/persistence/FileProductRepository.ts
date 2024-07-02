0d317452d0db5daa7b8927cb2443fc047e1731f0
Zuri Pabon <reach.zuripabon@gmail.com>
2024-07-02 21:10:41 +0200

    feat: file repository

--- apps/monolito/src/product/infrastructure/persistence/FileProductRepository.ts
+++ apps/monolito/src/product/infrastructure/persistence/FileProductRepository.ts
@@ -2,15 +2,15 @@ import { writeFileSync } from 'fs';
 import { join } from 'path';
 
 import { ProductRepository } from '@/product/domain/ProductRepository';
-import { Product } from '@/product/domain/Product';
+import { Product, ProductPrimitives } from '@/product/domain/Product';
 
 export class FileProductRepository implements ProductRepository {
   private static FILENAME = 'products.json';
 
-  private products: Product[] = [];
+  private products: ProductPrimitives[] = [];
 
   async save(product: Product): Promise<void> {
-    this.products.push(product);
+    this.products.push(product.toPrimitives());
     await writeFileSync(
       join(__dirname, FileProductRepository.FILENAME),
       JSON.stringify(this.products, null, 4)
